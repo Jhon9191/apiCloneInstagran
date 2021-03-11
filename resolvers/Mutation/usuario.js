cc277383db25095994aab85a7ccede253a241c39
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-11 16:58:03 -0300

    Creating a mutation to create a user

--- resolvers/Mutation/usuario.js
+++ resolvers/Mutation/usuario.js
@@ -5,7 +5,25 @@ module.exports = {
 
        
     novoUsuario: async (_, { dados }) => {
-        
+        const usuario = await Usuario.create(dados)
+        let idPerfis = []
+        if(dados.perfis){
+            for(filtro of dados.perfis){
+                const perfil = await buscar(_,{
+                    filtro
+                })
+                if(perfil){
+                    idPerfis.push(perfil._id)
+                }
+            }
+        }
+        for (juntar of idPerfis){
+            usuariosPerfis.create({
+                usuarioId: usuario._id,
+                perfilId: juntar
+            })
+        }
+        return usuario
     },
 
 
