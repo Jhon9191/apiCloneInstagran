b7ce183778e0679ce16b1b16859a6d91f0b1aef0
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-11 07:00:04 -0300

    Booting the Apollo server

---
+++ index.js
@@ -0,0 +1,31 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+
+type Query{
+    ola: String
+}
+
+
+`
+
+const resolvers = {
+
+    Query: {
+
+        ola(){
+            return "Hello graphql"
+        }
+
+    }
+
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen().then(({ url }) => {
+    console.log(`Executando na porta ${url}`)
+})
